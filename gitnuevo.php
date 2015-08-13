08d9368ba7b118dc1e4cad2b844369d213c34b69
andres potosi <apotosi@micrositios.net>
2015-08-13 09:02:51 -0500

    Se quita comentario y se agrega un div de 12 con parrafo

---
+++ gitnuevo.php
@@ -0,0 +1,20 @@
+ 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<div class="col-md-12">
+	
+	<p>se agrega una parrafo</p>
+
+
+</div>
+
+	
+</body>
+</html>
